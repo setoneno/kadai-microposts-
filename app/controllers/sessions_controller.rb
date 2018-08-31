609d65cc47aee61de085be2a88fe2664da0b476b
setoneno <setone0222@gmail.com>
2018-08-31 01:00:55 +0000

    post2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,8 +16,8 @@ class SessionsController < ApplicationController
 
   def destroy
      session[:user_id] = nil
-    flash[:success] = 'ログアウトしました。'
-    redirect_to root_url
+     flash[:success] = 'ログアウトしました。'
+     redirect_to root_url
   end
 
   private
