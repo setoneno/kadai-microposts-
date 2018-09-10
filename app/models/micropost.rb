d2725c77c9d064e22bf6d28848173b9384d0306a
setoneno <setone0222@gmail.com>
2018-09-10 14:31:32 +0000

    add like

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,10 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-
+  
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :likes, dependent: :destroy
+  has_many :like_users, through: :likes, source: :user
+  
 end
